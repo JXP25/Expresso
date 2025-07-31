b21152a72ffb7f3247b55220d24265744eba27d6
JaiPannu-IITI <pannujai06iiti@gmail.com>
2025-07-31 20:14:42 +0530

    Gauth route

--- frontend/src/helpers/auth.ts
+++ frontend/src/helpers/auth.ts
@@ -52,7 +52,7 @@ export async function login(data: LoginPostData) {
 
 export const loginWithGoogle = async (code: string) => {
   try {
-    const tokens = await apiCall(`/social/jwtsocial/jwt-pair-user`, {
+    const tokens = await apiCall(`/social/jwt/pair-user`, {
       method: "POST",
       body: {
         provider: "google-oauth2",
