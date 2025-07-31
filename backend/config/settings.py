f193c89ddddaa1c38335f5e0faf131766a79ab3b
JaiPannu-IITI <pannujai06iiti@gmail.com>
2025-07-31 19:34:24 +0530

    Cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -161,7 +161,13 @@ SOCIALACCOUNT_PROVIDERS = {
 
 REST_USE_JWT = True
 
-CORS_ALLOW_ALL_ORIGINS = True  
+
+CORS_ALLOWED_ORIGINS = [
+    FRONTEND_URL.rstrip("/"),      
+    "http://localhost:3000",        
+    "http://127.0.0.1:3000",       
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
